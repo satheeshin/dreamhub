ba5b24a8b7bb9fb7ddd2ee111cd102de68007b82
satheesh <satheesh@dreamstudio.in>
2020-10-10 11:21:17 +0530

    app

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class GitController extends Controller
+
 {
+
+
     public function index()
     {
+        $gitpass=config('web.github.pass');
         $data   = [];
-        $data[] = shell_exec("git pull https://satheeshdomainindia:Pass21zz@github.com/satheeshdomainindia/dreamhub.git");   
+        $data[] = shell_exec("git pull https://satheeshdomainindia:".$gitpass."@github.com/satheeshdomainindia/dreamhub.git");   
         dd($data);
     }
 
 
-    public function github()
-    {
-        $data[] = shell_exec("git pull https://satheeshdomainindia:Pass21zz@github.com/satheeshdomainindia/dreamhub.git");   
-        dd($data);
-        Log::info($data);
-    }
+   
 }
