085790cafcf7f8d19bbde2cda78a7e2cd009f19b
janu sath <janusath210@gmail.com>
2020-10-11 13:29:55 +0530

    change contact

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -147,7 +147,7 @@
 					</ul>
 					<ul class="nav-secondary">
 						<li>
-							<a href="contact.php"><i class="fas fa-phone icon-left"></i>+91 6379108040</a>
+							<a href="{{ cpurl('contact.php') }}"><i class="fas fa-phone icon-left"></i>+91 6379108040</a>
 						</li>
 						<li>
 						<a href="{{ cpurl('submitticket.php?step=2&deptid=1') }}"><i class="fas fa-comment icon-left"></i>Open Ticket</a>
