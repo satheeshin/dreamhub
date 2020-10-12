2103c29aa60430fcc9436cd52c64b72ce7a9e337
satheesh <satheesh@dreamstudio.in>
2020-10-12 22:12:03 +0530

    add command domain promo

---
+++ app/Console/Commands/updatePromoPrice.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Packages\Whmcs\WhmcsApi;
+
+class updatePromoPrice extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'promo:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update  Domain Promo Price';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(WhmcsApi $whmcsapi)
+    {
+        if ($this->confirm('Do you wish to Update Domain Promo Price ?')) {
+
+
+            $this->info('Display this on the screen');
+
+            $postarray=array(
+                'extension' => '.info',
+                'currency_code' => 'INR',
+                'register[1]' => '300',
+            );
+
+            $whmcsapi->get('CreateOrUpdateTLD', $postarray);
+
+            $domain=$postarray['extension'];
+
+            $this->info("$domain domain price updated on the screen ");
+        }
+    }
+}
