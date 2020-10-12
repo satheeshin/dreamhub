4b197a226abef3bff4fee1837c9280fc24b46246
satheesh <satheesh@dreamstudio.in>
2020-10-12 18:54:40 +0530

    app

--- app/Packages/Orderbox/MyOrderBox.php
+++ app/Packages/Orderbox/MyOrderBox.php
@@ -3,12 +3,48 @@
 namespace App\Packages\Orderbox;
 
 
-class MyOrderBox extends Baseapp
+class MyOrderBox 
 { 
+    protected $authentication ;
+
     public function __construct()
     {
-    	
+      $this->authentication= array(
+        'auth-userid' => config('web.orderbox.id'),
+        'api-key' => config('web.orderbox.key')
+      );
     }
+
+
+
+        public function getrecords($parms = array(), $url, $method = true)
+        {
+            echo gettype($this->authentication);
+            exit;
+            
+            $records=array_merge($parms, $this->authentication);
+
+            if ($method) 
+            {
+            $response = Http::timeout(2)->get('https://httpapi.com/api/'. $url, $records );
+            } 
+            else 
+            {
+            dd("http post method !!");
+            }
+            
+
+            if ($response->successful()) {
+            return ($response->body());
+            }
+            else{
+            dd($response->body());
+            }
+
+
+        }
+
+   
     public function customers($parms=array(),$link='resellers/search.json?')
     {
      return $this->getrecords($parms,$link);   
