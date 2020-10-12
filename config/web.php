2f842a616fd088eb0e0857f61db05e1d30246373
satheesh <satheesh@dreamstudio.in>
2020-10-12 20:43:44 +0530

    app

--- config/web.php
+++ config/web.php
@@ -15,7 +15,7 @@ return [
     'whmcs' => [
         'user' => env('WHMCS_USER', 'admin'),
         'pass' => env('WHMCS_PASS', ''),
-        'url'  => env('WHMCS_URL' , 'http://www.dreamhost.in/cp//includes/api.php'),
+        'url'  => env('WHMCS_URL' , 'http://88.198.75.149/cp/includes/api.php'),
     ],
 
 
